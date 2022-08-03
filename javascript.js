076f1ef76f1c80d308d3786fb50d8a1b81148678
arriann <arriann.lee@gmail.com>
2022-08-04 00:05:19 +0100

    First attempt at js code

---
+++ javascript.js
@@ -0,0 +1,65 @@
+let weapon = ['Rock','Paper','Scissors'];
+let getComputerChoice = weapon [Math.floor(Math.random()*weapon.length)];
+
+console.log(getComputerChoice);
+
+function playRound(playerSelection, computerSelection){
+if (playerSelection === computerSelection)
+  return 'Mind Reader!';
+}
+
+const playerSelection = 'Rock'
+const computerSelection = getComputerChoice;
+
+console.log(playRound(playerSelection,computerSelection));
+
+
+const r = 'Rock';
+const p = 'Paper';
+const s = 'Scissors';
+
+
+
+
+
+
+
+
+/*let play = prompt('Choose your weapon!');
+
+
+} else (playerSelection === computerSelection){
+  return 'Awesome, you won this one!';
+
+
+/*let choice = Rock, Paper, Scissors;
+
+function getComputerChoice() {
+  return (choice)
+} 
+
+console.log(getComputerChoice);
+
+
+let weapon = prompt ('Choose your weapon!');
+
+
+
+
+
+/*const Rock = 'a';
+const Paper = 'b';
+const Scissors = 'c';
+
+console.log(Rock);
+
+let choice(Rock, Paper,Scissors);
+
+
+let weapon = prompt ('Choose your weapon!');
+if (choice === choice) 'Mind reader - DRAW!';
+
+
+
+alert(weapon);
+*/
